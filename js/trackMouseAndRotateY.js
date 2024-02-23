93ac9c005e3c1724cef886af55697e62c0f2cdb1
Jonathan Schoonbroodt <hello@johndoesit.be>
2024-02-23 13:21:33 +0100

    wip

--- js/trackMouseAndRotateY.js
+++ js/trackMouseAndRotateY.js
@@ -5,45 +5,51 @@ perspective(2560px) rotateY(-30deg)
 à fond à droite 
 perspective(2560px) rotateY(30deg)
 
-position du curseur à droite axe x => coefficient = 1
-position du curseur au centre axe x => coefficient = 0
-position du curseur à gauche axe x => coefficient = -1
+position du curseur à droite axe x => coefficientX = 1
+position du curseur au centre axe x => coefficientX = 0
+position du curseur à gauche axe x => coefficientX = -1
 
 cursorPosition - centerOfContainer (containerWidth / 2) = resultToCalculateCoeffient
-coefficient = resultToCalculateCoeffient / containerWidth
-rectifiedCoefficient = coefficient * 2
-rotateY = 30 * rectifiedCoefficient
+coefficientX = resultToCalculateCoeffient / containerWidth
+rectifiedcoefficientX = coefficientX * 2
+rotateY = 30 * rectifiedcoefficientX
 
-attach event listener on mouse over to get coefficient and apply style for small and span inside our container
+attach event listener on mouse over to get coefficientX and apply style for small and span inside our container
 */
 
 const trackedContainer = document.getElementById('track-mouse-and-rotate-y')
 const trackAndRotate = document.getElementById('track-and-rotate')
 const containerWidth = trackedContainer.clientWidth
-const centralPosition = containerWidth / 2
+const containerHeight = trackedContainer.clientHeight
+const centralPositionWidth = containerWidth / 2
+const centralPositionHeight = containerHeight / 2
 const test = document.getElementById('test')
 let rotationValueY
 let rotationValueX
 
 trackedContainer.addEventListener('mousemove', (event) => {
   const containerRect = trackedContainer.getBoundingClientRect()
-  const cursorPosition = event.clientX - containerRect.left
-  const coefficient = ((cursorPosition - centralPosition) / containerWidth) * 2
+  const cursorPositionX = event.clientX - containerRect.left
+  const cursorPositionY = event.clientY - containerRect.top
+  const coefficientX = ((cursorPositionX - centralPositionWidth) / containerWidth) * 2
+  const coefficientY = ((cursorPositionY - centralPositionHeight) / containerHeight) * 2
   
-  test.innerText = coefficient
+  rotationValueY = (100 * coefficientY)
   
-  if (coefficient >= 0) {
+  test.innerText =  `${coefficientX} + ${coefficientY} + ${rotationValueY}`
+  
+  if (coefficientX >= 0) {
     trackAndRotate.style.transformOrigin = `right center`
-    rotationValueY = 0 + (60 * coefficient)
-    // rotationValueX = 0 + (60 * coefficient)
+    rotationValueX = 0 + (30 * coefficientX)
+    // rotationValueX = 0 + (60 * coefficientX)
   }
   
-  if (coefficient <= 0) {
+  if (coefficientX <= 0) {
     trackAndRotate.style.transformOrigin = `left center`
-    rotationValueY = 0 - (60 * coefficient)
-    // rotationValueX = 0 - (60 * coefficient)
+    rotationValueX = 0 - (30 * coefficientX)
+    // rotationValueX = 0 - (60 * coefficientX)
   }
   
-  trackAndRotate.style.transform = `rotateY(${rotationValueY}deg)`
+  trackAndRotate.style.transform = `rotateY(${rotationValueX}deg) rotateX(${rotationValueX}deg)`
   // trackAndRotate.style.transform = `rotateY(${rotationValueY}deg) rotateX(${rotationValueX}deg)`
 })
